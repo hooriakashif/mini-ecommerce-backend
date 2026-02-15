3b0208ad87380ea61f5e980e546f60805727ad07
hooriakashif <hooriakashif900@gmail.com>
2026-02-16 00:36:47 +0500

    Temporary SQLite for Railway deploy + psycopg binary

--- backend/settings.py
+++ backend/settings.py
@@ -84,15 +84,10 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'miniecommerce_db',         # the database you created
-        'USER': 'postgres',                 # ← change to this
-        'PASSWORD': '12345',                # ← your password
-        'HOST': 'localhost',
-        'PORT': '5432',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
-
 # Password validation
 # https://docs.djangoproject.com/en/6.0/ref/settings/#auth-password-validators
 
